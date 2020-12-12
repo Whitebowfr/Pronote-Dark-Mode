68e61b562308f072b8476f8ce8d05d7789e5a7d9
Whitebowfr <69417022+Whitebowfr@users.noreply.github.com>
2020-12-12 13:08:51 +0100

    Update base.js

--- base.js
+++ base.js
@@ -2,3 +2,8 @@ function store(name, value) {
     localStorage.setItem(name, value);
     console.log('"' + value + '" successfully stored in "' + name + '"');
 }
+
+function showSecondMenu() {
+    $('.objetBandeauEntete_secondmenu').show(); 
+    $("#id_34").show();
+}
