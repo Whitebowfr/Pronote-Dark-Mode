29edf02cea83b0329e70a4ca6f72f5bf160bc8f1
Whitebowfr <69417022+Whitebowfr@users.noreply.github.com>
2022-02-23 11:44:35 +0100

    Dernier fix avant sortie

--- extension/background.js
+++ extension/background.js
@@ -1,14 +1,14 @@
 chrome.extension.onConnect.addListener(function(port) {
     port.onMessage.addListener(function(msg) {
         if (msg.type == "post") {
-            fetch(`${msg.data.serverIp}/newData`, {
+            fetch(`${JSON.parse(msg.data).serverIp}/newData`, {
                 method: "POST",
                 headers: {'Content-Type': 'application/json'},
                 body: msg.data
             })
         } else {
             fetch(`${msg.data.serverIp}/getData?name=${msg.data.name}`, {
-                method: "GET",
+                method: "GET"
             }).then((res) => {
                 console.log(res)
                 return res.text().then((txt) => port.postMessage(txt))
