c990c0c64a741f5dc7aaff1c67dfc631c76ff8e2
Whitebowfr <69417022+Whitebowfr@users.noreply.github.com>
2022-02-25 15:54:47 +0100

    Ajout d'une notification en cas de version obsolète

--- extension/background.js
+++ extension/background.js
@@ -1,11 +1,31 @@
 chrome.extension.onConnect.addListener(function(port) {
     port.onMessage.addListener(function(msg) {
         if (msg.type != "post") {
-            fetch(`${msg.data.serverIp}/getData?name=${msg.data.name}`, {
+            fetch(`${msg.data.serverIp}/getData?name=${msg.data.name}&v=${chrome.app.getDetails().version}`, {
                 method: "GET"
             }).then((res) => {
-                console.log(res)
-                return res.text().then((txt) => port.postMessage(txt))
+                return res.text().then((txt) => {
+                    txt = JSON.parse(txt)
+                    console.log(txt)
+                    if (txt.needSmallUpdate) {
+                        chrome.browserAction.setBadgeText({text: "!!"})
+                        chrome.browserAction.setBadgeBackgroundColor({color: [255,0,0,255]})
+                    } else {
+                        chrome.browserAction.setBadgeText({text: ""})
+                        chrome.browserAction.setBadgeBackgroundColor({color: [255,0,0,255]})
+                    }
+                    if (txt.needBigUpdate) {
+                        chrome.notifications.create("PRONOTE_ID", {
+                            type: "basic",
+                            title: "Une mise à jour de Pronote Dark Mode est disponible",
+                            message: "Merci de l'effectuer pour éviter des crashs potentiels",
+                            iconUrl: "data/icon.png",
+                            silent: true,
+                        })
+                    }
+
+                    port.postMessage(txt)
+                })
             })
         }
     })
@@ -15,9 +35,8 @@ chrome.runtime.onMessage.addListener(function handleMessage(msg, sender, sendRes
     if (msg.type == "post") {
         fetch(`${JSON.parse(msg.data).serverIp}/newData`, {
             method: "POST",
-            headers: {'Content-Type': 'application/json'},
+            headers: { 'Content-Type': 'application/json' },
             body: msg.data
-        })    
-        console.log("[INFO] tried")
+        })
     }
 })
--- main-server.js
+++ main-server.js
@@ -2,6 +2,7 @@ var express = require('express')
 var app = express()
 var http = require('http').Server(app)
 var mysql = require('mysql')
+const fs = require('fs')
 require('dotenv').config()
 
 var pool = mysql.createPool({
@@ -62,21 +63,45 @@ app.post("/newData", (request, res) => {
 
 app.get("/getData", (request, response) => {
     const name = request.query.name
+    const userVersion = request.query.v.split(".").map(element => parseInt(element))
     response.set('Content-Type', 'text/plain');
-    pool.getConnection((err, conn) => {
-        if (err) console.log("[ERROR] " + err)
-        conn.query("SELECT * FROM users_settings WHERE name = ?", [name], (error, results) => {
-            conn.release()
-            if (error) console.log("[ERROR] " + error)
-            if (results.length == 0) {
-                response.send("{}")
-            } else {
-                response.send(JSON.stringify(results[0]).replaceAll(/[\\\\]"/gmi, '"').replaceAll(/"{/gm, '{').replaceAll(/}"/gm, "}"))
-            }
-            response.end()
+    fs.readFile(`${__dirname}/extension/manifest.json`, (err, data) => {
+        const currentVersion = JSON.parse(data).version.split(".").map(element => parseInt(element))
+        pool.getConnection((err, conn) => {
+            if (err) console.log("[ERROR] " + err)
+            conn.query("SELECT * FROM users_settings WHERE name = ?", [name], (error, results) => {
+                conn.release()
+                if (error) console.log("[ERROR] " + error)
+                if (results.length == 0) {
+                    if (checkIfBigUpdateNeeded(userVersion, currentVersion)) {
+                        response.send("{needBigUpdate: true}")
+                    } else if (checkIfSmallUpdateNeeded(userVersion, currentVersion)) {
+                        response.send("{needSmallUpdate: true}")
+                    } else {
+                        response.send("{}")
+                    }
+                } else {
+                    if (checkIfBigUpdateNeeded(userVersion, currentVersion)) {
+                        results[0].needBigUpdate = true;
+                    } else if (checkIfSmallUpdateNeeded(userVersion, currentVersion)) {
+                        results[0].needSmallUpdate = true;
+                    }
+                    response.send(JSON.stringify(results[0]).replaceAll(/[\\\\]"/gmi, '"').replaceAll(/"{/gm, '{').replaceAll(/}"/gm, "}"))
+                }
+                response.end()
+            })
         })
     })
+    
 })
+
+function checkIfBigUpdateNeeded(client, server) {
+    return client[0] < server[0] || server[1] - client[1] > 2
+}
+
+function checkIfSmallUpdateNeeded(client, server) {
+    return client[2] < server[2] || (server[1] - client[1] >= 1 && server[1] - client[1] <= 2)
+}
 
 http.listen(8003, "192.168.1.119", function() {
     console.log('listening on 8003')
