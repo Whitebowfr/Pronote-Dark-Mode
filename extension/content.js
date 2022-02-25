8adf564649e03020a8f8b51a4de44b6c6fd00e3c
Whitebowfr <69417022+Whitebowfr@users.noreply.github.com>
2022-02-25 15:54:23 +0100

    Ajout d'un nouveau thème

--- extension/content.js
+++ extension/content.js
@@ -75,6 +75,19 @@ var customThemes = {
         "theme-foncee": "#00faff",
         texteBaseColor: "#006dab",
         badConnectionBackground: `url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='1000' height='1000' viewBox='0 0 800 800'%3E%3Cg fill='none' stroke='%2375AEB4' stroke-width='1'%3E%3Cpath d='M769 229L1037 260.9M927 880L731 737 520 660 309 538 40 599 295 764 126.5 879.5 40 599-197 493 102 382-31 229 126.5 79.5-69-63'/%3E%3Cpath d='M-31 229L237 261 390 382 603 493 308.5 537.5 101.5 381.5M370 905L295 764'/%3E%3Cpath d='M520 660L578 842 731 737 840 599 603 493 520 660 295 764 309 538 390 382 539 269 769 229 577.5 41.5 370 105 295 -36 126.5 79.5 237 261 102 382 40 599 -69 737 127 880'/%3E%3Cpath d='M520-140L578.5 42.5 731-63M603 493L539 269 237 261 370 105M902 382L539 269M390 382L102 382'/%3E%3Cpath d='M-222 42L126.5 79.5 370 105 539 269 577.5 41.5 927 80 769 229 902 382 603 493 731 737M295-36L577.5 41.5M578 842L295 764M40-201L127 80M102 382L-261 269'/%3E%3C/g%3E%3Cg fill='%2300FAFF'%3E%3Ccircle cx='769' cy='229' r='9'/%3E%3Ccircle cx='539' cy='269' r='9'/%3E%3Ccircle cx='603' cy='493' r='9'/%3E%3Ccircle cx='731' cy='737' r='9'/%3E%3Ccircle cx='520' cy='660' r='9'/%3E%3Ccircle cx='309' cy='538' r='9'/%3E%3Ccircle cx='295' cy='764' r='9'/%3E%3Ccircle cx='40' cy='599' r='9'/%3E%3Ccircle cx='102' cy='382' r='9'/%3E%3Ccircle cx='127' cy='80' r='9'/%3E%3Ccircle cx='370' cy='105' r='9'/%3E%3Ccircle cx='578' cy='42' r='9'/%3E%3Ccircle cx='237' cy='261' r='9'/%3E%3Ccircle cx='390' cy='382' r='9'/%3E%3C/g%3E%3C/svg%3E")`
+    },
+    PATCHWORK: {
+        colorMain: "#791f2b",
+        colorMenu: "#9c597f",
+        colorMenuBis: "#b17d9a",
+        colorWidget: "#a05774",
+        colorTitles: "#000000",
+        colorSubMenu: "#e96912",
+        backgroundUrl: 'url("https://wallpaperaccess.com/full/722715.jpg")',
+        colorAccent: "#f49f2b",
+        "theme-foncee": "#e39ec5",
+        texteBaseColor: "#000000",
+        badConnectionBackground: `url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='1000' height='1000' viewBox='0 0 800 800'%3E%3Cg fill='none' stroke='%2375AEB4' stroke-width='1'%3E%3Cpath d='M769 229L1037 260.9M927 880L731 737 520 660 309 538 40 599 295 764 126.5 879.5 40 599-197 493 102 382-31 229 126.5 79.5-69-63'/%3E%3Cpath d='M-31 229L237 261 390 382 603 493 308.5 537.5 101.5 381.5M370 905L295 764'/%3E%3Cpath d='M520 660L578 842 731 737 840 599 603 493 520 660 295 764 309 538 390 382 539 269 769 229 577.5 41.5 370 105 295 -36 126.5 79.5 237 261 102 382 40 599 -69 737 127 880'/%3E%3Cpath d='M520-140L578.5 42.5 731-63M603 493L539 269 237 261 370 105M902 382L539 269M390 382L102 382'/%3E%3Cpath d='M-222 42L126.5 79.5 370 105 539 269 577.5 41.5 927 80 769 229 902 382 603 493 731 737M295-36L577.5 41.5M578 842L295 764M40-201L127 80M102 382L-261 269'/%3E%3C/g%3E%3Cg fill='%2300FAFF'%3E%3Ccircle cx='769' cy='229' r='9'/%3E%3Ccircle cx='539' cy='269' r='9'/%3E%3Ccircle cx='603' cy='493' r='9'/%3E%3Ccircle cx='731' cy='737' r='9'/%3E%3Ccircle cx='520' cy='660' r='9'/%3E%3Ccircle cx='309' cy='538' r='9'/%3E%3Ccircle cx='295' cy='764' r='9'/%3E%3Ccircle cx='40' cy='599' r='9'/%3E%3Ccircle cx='102' cy='382' r='9'/%3E%3Ccircle cx='127' cy='80' r='9'/%3E%3Ccircle cx='370' cy='105' r='9'/%3E%3Ccircle cx='578' cy='42' r='9'/%3E%3Ccircle cx='237' cy='261' r='9'/%3E%3Ccircle cx='390' cy='382' r='9'/%3E%3C/g%3E%3C/svg%3E")`
     }
 }
 
@@ -319,7 +332,6 @@ if (!localStorage.getItem("pronoteDarkModeOptions")) {
 isInStorage("syncWithServer", true)
 
 function replaceLocalStorageWithSynced(sync) {
-    sync = JSON.parse(sync)
     let og = localStorage.getItem("pronoteDarkModeOptions");
     og = JSON.parse(og);
     og.ogName = sync.name ?? "";
@@ -500,7 +512,7 @@ function addLinks() {
             window.clearTimeout(devoirsTimeOut)
             var devoirArray = document.getElementsByClassName('descriptif');
             for (var i = 0; i < devoirArray.length; i++) {
-                var splitCurrentDevoir = devoirArray[i].firstElementChild.innerText.match(/(https?:\/\/[^\s]+)/g)
+                var splitCurrentDevoir = devoirArray[i].firstElementChild.innerText.match(/((https|http)?:\/\/[^\s]+)/g)
                 if (splitCurrentDevoir != null) {
                     for (var j = 0; j < splitCurrentDevoir.length; j++) {
                         devoirArray[i].firstElementChild.innerHTML = devoirArray[i].firstElementChild.innerHTML.replace(splitCurrentDevoir[j], "[Link]".link(splitCurrentDevoir[j]))
@@ -906,6 +918,15 @@ function addOptionHTML() {
                     <div class="previewColor" style="background-color:#00faff"></div>
                     <div class="previewColor" style="background-color:#3566e0"></div>
                     <input type="button" style="margin-left: 1rem;" value="Appliquer" class="customButtons" onclick="changeCustomTheme('NEON RIDER')">
+                </div>
+                <div>
+                    <p style="display: inline-block;" class="customThemeName">Patchwork : </p>
+                    <div class="previewColor" style="background-color:#791f2b"></div>
+                    <div class="previewColor" style="background-color:#a05774"></div>
+                    <div class="previewColor" style="background-color:#b17d9a"></div>
+                    <div class="previewColor" style="background-color:#f49f2b"></div>
+                    <div class="previewColor" style="background-color:#e96912"></div>
+                    <input type="button" style="margin-left: 1rem;" value="Appliquer" class="customButtons" onclick="changeCustomTheme('PATCHWORK')">
                 </div>
             </div>
         </div>
